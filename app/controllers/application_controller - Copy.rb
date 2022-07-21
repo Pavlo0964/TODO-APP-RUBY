4399718fd10f57d3a37721c4e5ca9a69a88c3c4e
Jean0125 <business89106@gmail.com>
2022-07-21 05:03:15 -0400

    Update

--- app/controllers/application_controller - Copy.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  rescue_from CanCan::AccessDenied do |exception|
-    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
-    ## to avoid deprecation warnings with Rails 3.2.x (and incidentally using Ruby 1.9.3 hash syntax)
-    ## this render call should be:
-    # render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
-  end
-end
